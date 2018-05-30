982d65b634e4eb472aedc500b967222b4111187e
Adam Julier <adamjulier@ool-ad020067.dyn.optonline.net>
2018-05-30 16:25:46 -0400

    nav module working

--- components/partials/Navbar.js
+++ components/partials/Navbar.js
@@ -15,14 +15,28 @@ import {
     } from 'react-native';
 import { Constants } from 'expo';
 
+import NavigationBar from 'react-native-navbar';
+
 export default class Navbar extends Component {
 
     render() {
+
+      // const rightButtonConfig = {
+      //   title: 'Next',
+      //   handler: () => alert('hello!'),
+      // };
+
       return (
 
         <View style={ styles.navbar } >
 
-          <Text>Navbar</Text>
+          <NavigationBar style={ styles.navigation }
+            title={{ title: 'Main' }}
+            leftButton={{ title: 'Left' }}
+            rightButton={{ title: 'Right' }}
+          />
+
+          {/* rightButton={rightButtonConfig} */}
 
       </View>
 
@@ -32,8 +46,15 @@ export default class Navbar extends Component {
 }
 
 const styles = StyleSheet.create({
+  navigation: {
+    backgroundColor: 'white',
+  },
   navbar: {
-    width: 450,
+    backgroundColor: 'white',
+    paddingLeft: 50,
+    paddingRight: 50,
     borderWidth: 1,
+    flex: 1,
+    borderRadius: 20,
   }
 })
